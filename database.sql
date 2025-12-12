0d6211c4183670948cbae4eb9ec7373befcffbfe
Ilya_M <iliya.miroshnichenko@yandex.ru>
2025-12-13 02:54:28 +0300

    updated tables. Made references

--- database.sql
+++ database.sql
@@ -1,38 +1,51 @@
 CREATE TABLE Lessons (
-    id SMALLINT PRIMARY KEY,
-    lesson_name VARCHAR(100) NOT NULL,
-    lesson_content ,
-    video_url VARCHAR(100) NOT NULL,
-    course_position ,
-    created_at TIMESTAMPZ DEFAULT NOW(),
-    updated_at TIMESTAMPZ DEFAULT NOW(),
-    course_url VARCHAR(100) NOT NULL,
+    id SERIAL PRIMARY KEY,
+    course_id INT NOT NULL REFERENCES Courses(id)
+    name VARCHAR(100) NOT NULL,
+    content TEXT NOT NULL,
+    video_url VARCHAR(255) NOT NULL,
+    position SMALLINT NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW(),
+    course_url VARCHAR(255) NOT NULL,
     deleted BOOLEAN
-)
+);
 
 CREATE TABLE Courses (
-    id SMALLINT PRIMARY KEY,
-    course_name VARCHAR(100) NOT NULL,
-    course_discription TEXT,
-    created_at TIMESTAMPZ DEFAULT NOW(),
-    updated_at TIMESTAMPZ DEFAULT NOW(),
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    discription TEXT,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW(),
     deleted BOOLEAN
-)
+);
 
 CREATE TABLE Modules (
-    id SMALLINT PRIMARY KEY,
-    module_name VARCHAR(100) NOT NULL,
-    module_discription TEXT,
-    created_at TIMESTAMPZ DEFAULT NOW(),
-    updated_at TIMESTAMPZ DEFAULT NOW(),
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    discription TEXT,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW(),
     deleted BOOLEAN
-)
+);
 
 CREATE TABLE Programs (
-    id SMALLINT PRIMARY KEY,
-    program_name VARCHAR(100) NOT NULL,
-    program_price FLOAT,
-    program_type VARCHAR(100) NOT NULL,
-    created_at TIMESTAMPZ DEFAULT NOW(),
-    updated_at TIMESTAMPZ DEFAULT NOW()
-)
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    price DECIMAL (10, 2),
+    type VARCHAR(100) NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW()
+);
+
+CREATE TABLE programs_modules(
+    program_id INT NOT NULL REFERENCES Programs(id),
+    module_id INT NOT NULL REFERENCES Modules(id),
+    PRIMARY KEY (program_id, module_id)
+);
+
+CREATE TABLE modules_courses(
+    module_id INT NOT NULL REFERENCES Modules(id),
+    course_id INT NOT NULL REFERENCES Courses(id),
+    PRIMARY KEY (module_id, course_id)
+);
